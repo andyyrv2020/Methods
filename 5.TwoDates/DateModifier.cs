c36f33c3ba84944e182236cca1bcc7bb2a0182cb
Andy Rashev <46199572+andyyrv2020@users.noreply.github.com>
2023-11-22 00:06:37 +0200

    Add files via upload

---
+++ 5.TwoDates/DateModifier.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class DateModifier
+{
+    public static void Main()
+    {
+        string date1 = "1992 05 31";
+        string date2 = "2016 06 17";
+
+        int difference = CalculateDateDifference(date1, date2);
+
+        Console.WriteLine(difference);
+    }
+
+    public static int CalculateDateDifference(string dateString1, string dateString2)
+    {
+        DateTime date1 = DateTime.ParseExact(dateString1, "yyyy MM dd", System.Globalization.CultureInfo.InvariantCulture);
+        DateTime date2 = DateTime.ParseExact(dateString2, "yyyy MM dd", System.Globalization.CultureInfo.InvariantCulture);
+
+        TimeSpan difference = date2 - date1;
+
+        return difference.Days;
+    }
+}
---
+++ 5.TwoDates/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _5.TwoDates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
