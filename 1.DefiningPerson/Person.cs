6a9942227d4a0189868f7fd1c6e45e51feb4d738
Andy Rashev <46199572+andyyrv2020@users.noreply.github.com>
2023-11-22 00:04:55 +0200

    Add files via upload

---
+++ 1.DefiningPerson/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Person
+{
+    private string name;
+    private int age;
+
+    // Свойство за достъп до полето name
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    // Свойство за достъп до полето age
+    public int Age
+    {
+        get { return age; }
+        set { age = value; }
+    }
+
+    // Метод за представяне
+    public void IntroduceYourself()
+    {
+        Console.WriteLine($"Hello, my name is {Name} and I am {Age} years old.");
+    }
+}
---
+++ 1.DefiningPerson/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace DefiningPerson
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Person firstPerson = new Person();
+            firstPerson.Name = "Gosho";
+            firstPerson.Age = 30;
+
+            firstPerson.IntroduceYourself();
+        }
+    }
+}
